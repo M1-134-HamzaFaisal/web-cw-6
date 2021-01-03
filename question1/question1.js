bb4186e63bd3f5d266670100a227f796a33336ef
M1-134-HamzaFaisal <76451177+M1-134-HamzaFaisal@users.noreply.github.com>
2021-01-03 21:51:09 +0300

    done

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,12 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+let Name =prompt(`whats your name?`);
+console.log(Name)
+
+function sayHello(){
+    alert(`Hello,${Name}`);
+}
+if(Name){
+    sayHello()
+}else{
+    alert(`Invalid Input`)
+}
